540edfa33449d2b48fb6c8e10a4f18a83c75b1dc
brizay1211-sketch <brizay1211@gmail.com>
2026-03-19 22:58:29 -0400

    Add files via upload

---
+++ L26.1.p.c1.defg.devs.Ej6.ent1/dist/models/Cl_mPersonal.js
@@ -0,0 +1,33 @@
+export default class Cl_mPersonal {
+    _nombre = "";
+    _tipo = 0;
+    _salarioBase = 0;
+    _bono = 0;
+    _peso = 0;
+    constructor({ nombre, tipo, salario, bono, peso } = {
+        nombre: "",
+        tipo: 0,
+        salario: 0,
+        bono: 0,
+        peso: 0,
+    }) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.salarioBase = salario;
+        this.bono = bono;
+    }
+    set nombre(nombre) { this._nombre = nombre; }
+    get nombre() { return this._nombre; }
+    set tipo(tipo) { this._tipo = +tipo; }
+    get tipo() { return this._tipo; }
+    set salarioBase(salario) { this._salarioBase = +salario; }
+    get salarioBase() { return this._salarioBase; }
+    set bono(bono) { this._bono = +bono; }
+    get bono() { return this._bono; }
+    set peso(peso) { this._peso = +peso; }
+    get peso() { return this._peso; }
+    get salarioTotal() {
+        return this.salarioBase + this.bono;
+    }
+}
+//# sourceMappingURL=Cl_mPersonal.js.map
