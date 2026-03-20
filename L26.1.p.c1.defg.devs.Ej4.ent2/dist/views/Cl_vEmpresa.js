aa59058161f9910b8d45185b52e858e1451c9f60
brizay1211-sketch <brizay1211@gmail.com>
2026-03-19 23:07:00 -0400

    Add files via upload

---
+++ L26.1.p.c1.defg.devs.Ej4.ent2/dist/views/Cl_vEmpresa.js
@@ -0,0 +1,21 @@
+export default class Cl_vEmpresa {
+    lblPromedioObreros;
+    btProcesarPersonal;
+    lblPromedioAdministrativos;
+    lblTotalObreros;
+    lblTotalAdministrativos;
+    constructor() {
+        this.lblPromedioObreros = document.getElementById("body_lblPromedioObreros");
+        this.btProcesarPersonal = document.getElementById("body_btProcesarPersonal");
+        this.lblPromedioAdministrativos = document.getElementById("body_lblPromedioAdministrativos");
+        this.lblTotalObreros = document.getElementById("body_lblTotalObreros");
+        this.lblTotalAdministrativos = document.getElementById("body_lblTotalAdministrativos");
+    }
+    reportar({ empresa, personal }) {
+        this.lblPromedioObreros.innerHTML = `${empresa.PromedioObreros}`;
+        this.lblPromedioAdministrativos.innerHTML = `${empresa.PromedioAdministrativos}`;
+        this.lblTotalObreros.innerHTML = `${empresa.totalobreros}`;
+        this.lblTotalAdministrativos.innerHTML = `${empresa.totalAdministrativos}`;
+    }
+}
+//# sourceMappingURL=Cl_vEmpresa.js.map
