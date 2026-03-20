e9a6b563df3e24307b3c6377e8c01c5e25c042ca
brizay1211-sketch <brizay1211@gmail.com>
2026-03-19 22:59:34 -0400

    Add files via upload

---
+++ L26.1.p.c1.defg.devs.Ej6.ent2/L26.1.p.c1.defg.devs.Ej6.ent1/dist/views/Cl_vPersonalBootstrap.js
@@ -0,0 +1,34 @@
+export default class Cl_vPersonalBootstrap {
+    inNombre;
+    inTipo;
+    inSalario;
+    inBono;
+    btCancelar;
+    btAceptar;
+    modal;
+    constructor() {
+        this.inNombre = document.getElementById("personal_inNombre");
+        this.inTipo = document.getElementById("personal_inTipo");
+        this.inSalario = document.getElementById("personal_inSalario");
+        this.inBono = document.getElementById("personal_inBono");
+        this.btCancelar = document.getElementById("personal_btCancelar");
+        this.btAceptar = document.getElementById("personal_btAceptar");
+        const elementoModal = document.getElementById("personal");
+        this.modal = new bootstrap.Modal(elementoModal);
+    }
+    get nombre() { return this.inNombre.value; }
+    get tipo() { return +this.inTipo.value; }
+    get salario() { return +this.inSalario.value; }
+    get bono() { return +this.inBono.value; }
+    onAceptar(callback) { this.btAceptar.onclick = callback; }
+    onCancelar(callback) { this.btCancelar.onclick = callback; }
+    mostrar() {
+        this.inNombre.value = "";
+        this.inTipo.value = "";
+        this.inSalario.value = "";
+        this.inBono.value = "0";
+        this.modal.show();
+    }
+    ocultar() { this.modal.hide(); }
+}
+//# sourceMappingURL=Cl_vPersonalBootstrap.js.map
